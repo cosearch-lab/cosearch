5a64127d0340970b542b7c21d8ca579b6ca81373
Tristan Stérin <cosmo@cosm3.lan>
2024-07-15 12:24:14 +0200

    reviews

---
+++ src/routes/[contrib]/reviews/+page.svelte
@@ -0,0 +1,18 @@
+<div class="text-lg font-bold">Reviews</div>
+
+<table class="w-2/3 text-xs border-collapse border border-slate-400">
+	<thead class="text-left">
+		<th class="border border-slate-300 p-1">Reviewers</th>
+		<th class="border border-slate-300 p-1">URL</th>
+		<th class="border border-slate-300 p-1">Notes</th>
+	</thead>
+	<tbody>
+		<tr>
+			<td class="border border-slate-300 p-1"><a href="/">yforster</a></td>
+			<td class="border border-slate-300 p-1">
+				<a href="https://github.com/ccz181078/Coq-BB5">https://github.com/ccz181078/Coq-BB5</a>
+			</td>
+			<td class="border border-slate-300 p-1"></td>
+		</tr>
+	</tbody>
+</table>
