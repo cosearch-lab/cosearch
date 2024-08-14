1c924badeaa220c9057ff17e4000b18137a8c943
Tristan Stérin <cosmo@cosm3.local>
2024-08-14 17:02:43 +0100

    APACHE license

---
+++ src/routes/contribution/[contribution]/reviews/+layout.svelte
@@ -0,0 +1,9 @@
+<p class="mt-2 text-sm">
+	<strong><em>Information:</em></strong> the review system is currently extremely minimal. Any ideas
+	for improvement are welcome. <a><i class="fa-brands fa-github"></i></a>
+	<a href="https://discord.com/channels/960643023006490684/1273310257069690994"
+		><i class="fa-brands fa-discord"></i></a
+	>
+</p>
+
+<slot />
