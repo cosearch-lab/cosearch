8029182467b7620e30981c9e25526379a9136672
Tristan Stérin <cosmo@cosm3.local>
2024-08-14 17:07:03 +0100

    disclaimer cosearch github link

--- src/routes/contribution/[contribution]/reviews/+layout.svelte
+++ src/routes/contribution/[contribution]/reviews/+layout.svelte
@@ -1,6 +1,7 @@
 <p class="mt-2 text-sm">
 	<strong><em>Information:</em></strong> the review system is currently extremely minimal. Any ideas
-	for improvement are welcome. <a><i class="fa-brands fa-github"></i></a>
+	for improvement are welcome.
+	<a href="https://github.com/cosearch-lab/cosearch/issues"><i class="fa-brands fa-github"></i></a>
 	<a href="https://discord.com/channels/960643023006490684/1273310257069690994"
 		><i class="fa-brands fa-discord"></i></a
 	>
